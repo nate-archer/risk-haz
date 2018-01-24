448a00bb7e29042947354b7f4b591a3fa10ec157
Nate Archer <35746240+nate-archer@users.noreply.github.com>
2018-01-23 16:57:19 -0800

    Added Message Sent Confirmation page

---
+++ www/contact/message-sent/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		
+		<link rel="stylesheet" href="http://www.riskhaz.com/css/styles.css">
+		
+		<title>Risk & Hazard Message Sent</title>
+	</head>
+    <body>
+	    <div class="container nav-container">
+			<nav class="navbar navbar-toggleable navbar-light nav-element">
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<a class="navbar-brand" id="site-name" href="http://www.riskhaz.com/calculator">RiskHaz</a>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto justify-content-end">
+						<li class="nav-item">
+							<a class="nav-link" href="http://www.riskhaz.com/calculator">Calculator</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="http://www.riskhaz.com/about">About</a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="http://www.riskhaz.com/contact">Contact <span class="sr-only">(current)</span></a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		
+		<div class="container-fluid top-border">
+		</div>
+		
+		<div class="container">
+		    <div class="row content-label">
+			    <p class="h5">Contact</p>
+			</div>
+			
+			<div class="row content-label">
+			    <p>Thank you. Your message has been sent.</p>
+			</div>
+			
+			
+		</div>
+		
+		
+			
+        <!-- jQuery first, then Tether, then Bootstrap JS. -->
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+	</body>
+</html>
